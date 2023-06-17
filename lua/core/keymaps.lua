49df20efbcdf4b769566377075a1b6dc01aa0aec
cloud-np <cloud-np@outlook.com>
2023-06-18 01:38:05 +0300

    feat(keymaps): add keymap to close buffers

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -22,4 +22,7 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- Close current buffer
+vim.api.nvim_set_keymap('n', '<C-w>', ':bd<CR>', { silent = true, noremap = true })
+
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
