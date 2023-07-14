f2ac18eea480189bdf08682b6cd8b8a5bf7124d5
cloud-np <cloud-np@outlook.com>
2023-07-14 22:30:41 +0300

    feat(completions): add vim like navigation to the auto complete pop up window

--- lua/core/plugin_config/completions.lua
+++ lua/core/plugin_config/completions.lua
@@ -5,6 +5,8 @@ require("luasnip.loaders.from_vscode").lazy_load()
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-j>'] = cmp.mapping.select_next_item(),
+      ['<C-k>'] = cmp.mapping.select_prev_item(),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-o>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
