2900b185e69179030034cd42da3e9c5f3f85f30d
cloud-np <cloud-np@outlook.com>
2024-03-31 20:16:47 +0300

    feat: add vscode support

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
-require("core.options")
-require("core.keymaps")
-require("core.plugins")
-require("core.autocommands")
-require("core.plugin_config")
+if vim.g.vscode then
+    -- There are not working why?
+    require("vscode.settings")
+    -- vim.api.nvim_set_keymap('n', '<Space>', ":call VSCodeNotify('whichkey.show')<CR>", {})
+    -- vim.api.nvim_set_keymap('x', '<Space>', ":call VSCodeNotify('whichkey.show')<CR>", {})
+else
+    require("core.options")
+    require("core.keymaps")
+    require("core.plugins")
+    require("core.autocommands")
+    require("core.plugin_config")
+end
