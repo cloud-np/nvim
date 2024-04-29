af036b3a31dd3e40bfaf3e860cf3c32d19cf8c9f
cloud-np <cloud-np@outlook.com>
2024-04-29 20:16:50 +0300

    chore: update null-ls

--- lua/core/plugin_config/null-ls.lua
+++ lua/core/plugin_config/null-ls.lua
@@ -5,7 +5,7 @@ null_ls.setup({
         null_ls.bultins.diagnostics.mypy,
         null_ls.bultins.diagnostics.ruff,
         null_ls.bultins.formatting.prettier.with({
-            filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+            filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "html", "css" },
             args = { "--tabs-width", "4", "--use-tabs", "true", "--no-semi", "false", "--single-quote", "true", "--trailing-comma", "all", "--bracket-spacing", "true", "--arrow-parens", "always", "--print-width", "120" },
         }),
     }
